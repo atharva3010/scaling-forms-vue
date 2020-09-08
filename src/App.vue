2daa0eafd888458eabbcd420ed32d2aa16f63f97
Atharva Sharma <mailatharvasharma@gmail.com>
2020-09-08 20:49:24 +0530

    chore: rounded-lg

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     class="min-h-screen min-h-screen-ios-fix w-full grid-place-center p-7.5"
   >
     <div
-      class="flex flex-col py-7.5 px-5 md:px-7.5 max-w-md rounded w-full bg-white shadow-2xl"
+      class="flex flex-col py-7.5 px-5 md:px-7.5 max-w-md rounded-lg w-full bg-white shadow-2xl"
     >
       <div class="max-w-2xl mx-auto w-full">
         <transition name="fade" mode="out-in">
